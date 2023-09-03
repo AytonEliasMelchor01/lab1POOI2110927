6bf46f5d1ce5a8f5e3b5bf8ecb6bdf7f4d33c870
AytonEliasMelchor01 <86389838+AytonEliasMelchor01@users.noreply.github.com>
2023-09-02 22:45:33 -0500

    Add files via upload

---
+++ Ejercicio-4.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    //Ejercicio 3
+	    
+	    double numA= 2 ; double numB = 3;
+	    if ((numA >= 0 && numB >= 0) || (numA < 0 && numB < 0)) 
+            System.out.println("true");
+        else
+            System.out.println("false");
+
+        
+
+	    
+	    //Resultado: true , por ser numeros con los mismos signos 
+	}
+}
