5eea17092fc4ac7a8c504c0c9ca37a520efa7f66
AytonEliasMelchor01 <86389838+AytonEliasMelchor01@users.noreply.github.com>
2023-09-02 22:42:04 -0500

    Add files via upload

---
+++ Ejercicio_3.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    //Ejercicio 3
+	    
+	    double numA= 2 ; double numB = 3;
+	    if (numA > 0 && numB > 0)
+            System.out.println("true");
+        else
+            System.out.println("Al menos 1 es negativo");
+        
+
+	    
+	    //Resultado: true , por ser 2 y 3 numeros enteros positivos 
+	}
+}
