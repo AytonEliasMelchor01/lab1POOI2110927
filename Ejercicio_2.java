d4cdf8750a0f3efffc3a39403ae98e23d1a7f749
AytonEliasMelchor01 <86389838+AytonEliasMelchor01@users.noreply.github.com>
2023-09-02 22:35:39 -0500

    Add files via upload

---
+++ Ejercicio_2.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    //Ejercicio 2
+	    
+	    int num=1;
+	    if (num > 0 && num % 2 != 0) 
+            System.out.println("true");
+	    
+	    //Resultado: true , por ser 1 un número impar positivo 
+	}
+}
